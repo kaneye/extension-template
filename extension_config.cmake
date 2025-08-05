c9147a382c608903b766083a2309ce46f19f194e
kaneye <87104692+kaneye@users.noreply.github.com>
2025-08-05 14:11:35 +0800

    Update extension_config.cmake

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,7 +1,8 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
 
 # Extension from this repo
-duckdb_extension_load(quack
+#duckdb_extension_load(quack
+duckdb_extension_load(kdb
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
 )
