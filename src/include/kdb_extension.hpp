039619cf7c2800ea99e7444e5bc4e36891e4f3fd
kaneye <87104692+kaneye@users.noreply.github.com>
2025-08-05 13:57:52 +0800

    Add files via upload

---
+++ src/include/kdb_extension.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "duckdb.hpp"
+
+namespace duckdb {
+
+class KdbExtension : public Extension {
+public:
+	void Load(DuckDB &db) override;
+	std::string Name() override;
+	std::string Version() const override;
+};
+
+
+} // namespace duckdb
