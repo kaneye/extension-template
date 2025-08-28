01846fb11f1a8a1efcfd2b30b1d91d9ac0fb7d75
kaneye <87104692+kaneye@users.noreply.github.com>
2025-08-28 10:44:30 +0800

    Update qfile.cpp

--- src/kdb_reader/qfile.cpp
+++ src/kdb_reader/qfile.cpp
@@ -30,6 +30,7 @@ kdb::BinFile::BinFile(FILE *fp)
   :filename_(""), fp_(fp) {}
 
 kdb::BinFile::~BinFile() {
+  std::cout<<"debug,~BinFile()"<<std::endl;
     // if(fp_) {
     //     fclose(fp_);
     // }
