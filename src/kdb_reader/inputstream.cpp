33be9f7da9b32c0b9f20bef7bdcc74c2d03b9459
kaneye <87104692+kaneye@users.noreply.github.com>
2025-09-02 17:22:02 +0800

    Update inputstream.cpp

--- src/kdb_reader/inputstream.cpp
+++ src/kdb_reader/inputstream.cpp
@@ -47,7 +47,7 @@ size_t filestream::get_length() {
 filestream::filestream(const std::string &path): path_(path),fp_(nullptr) {}
 filestream::~filestream() {
     fclose(this->fp_);
-    std::cout<<"debug,~filestream()"<<std::endl;
+    //std::cout<<"mem_debug,~filestream()"<<std::endl;
 }
 
 
